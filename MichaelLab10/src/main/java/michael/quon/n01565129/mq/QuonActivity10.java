25d7af2f53524b7eac4d0de28c31a70e4b5286cb
Michael-Quon <michaelquon08@gmail.com>
2024-03-21 21:17:30 -0400

    Made the bottom navigation

--- MichaelLab10/src/main/java/michael/quon/n01565129/mq/QuonActivity10.java
+++ MichaelLab10/src/main/java/michael/quon/n01565129/mq/QuonActivity10.java
@@ -1,15 +1,70 @@
 // Michael Quon N01565129
 package michael.quon.n01565129.mq;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class QuonActivity10 extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class QuonActivity10 extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.MicBottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.Mic_Michael);
     }
+
+    Mi1chaelFragment mi1chaelFragment = new Mi1chaelFragment();
+//    PersonFragment personFragment = new PersonFragment();
+  //  SettingsFragment settingsFragment = new SettingsFragment();
+    //MichaelFragment michaelFragment = new MichaelFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.Mic_Michael) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.MicFlFragment, mi1chaelFragment)
+                    .commit();
+            return true;
+        }
+
+//        if (itemId == R.id.Mic_Quon) {
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//               //     .replace(R.id.MicFlFragment, qu2onFragment)
+//                    .commit();
+//            return true;
+//        }
+//
+//        if (itemId == R.id.Mic_N01565129) {
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                //    .replace(R.id.MicFlFragment, n013565129Fragment)
+//                    .commit();
+//            return true;
+//        }
+//
+//        if (itemId == R.id.Mic_MQ) {
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//              //      .replace(R.id.MicFlFragment, m4qFragment)
+//                    .commit();
+//            return true;
+//        }
+
+        return false;
+    }
+
 }
